3b36817ad5da84ed3a74baba5f81cf253dd18a99
qtn2932 <quan.nguyen199712@gmail.com>
2019-03-20 22:52:51 -0400

    added more route to get data

--- api/server.js
+++ api/server.js
@@ -116,9 +116,158 @@ const getCategories= ()=>{
         })
       })
 }
+
+const getChapterByState= (state)=>{
+    
+  return new Promise((resolve, reject) => {
+      // Setup empty array to store results
+      
+      const chaptersRecord = []
+  
+      // Query
+      const apiQuery = {
+        pageSize: 100,
+      }
+  
+      // Go get it!
+      base('States').select(apiQuery).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+        // The properties here would correspond to your records
+
+        records.forEach(record=> {
+          console.log(record.fields.FullName);
+          console.log(state)
+          if(record.fields.FullName==state){
+            console.log("got here")
+            console.log(record.fields.ChapterName)
+            const post ={
+              Name: record.fields.ChapterName,
+          }
+          chaptersRecord.push(post);
+          }
+          
+  
+          // Store each result in our empty array
+          
+        })
+  
+        fetchNextPage()
+      }, function done(error) {
+        // Throw error if exists
+        if (error) reject({ error })
+  
+        // Finish
+        resolve(chaptersRecord)
+      })
+    })
+}
+const getServiceByChapter= (chapter)=>{
+    
+  return new Promise((resolve, reject) => {
+      // Setup empty array to store results
+      
+      const chaptersRecord = []
+  
+      // Query
+      const apiQuery = {
+        pageSize: 100,
+      }
+  
+      // Go get it!
+      base('Chapters').select(apiQuery).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+        // The properties here would correspond to your records
+
+        records.forEach(record=> {
+          console.log(record.fields.Chapters)
+          if(record.fields.Chapters==chapter){
+            const post ={
+              Service: record.fields.ServiceName
+          }
+          chaptersRecord.push(post)
+          }
+          
+  
+          // Store each result in our empty array
+          
+        })
+  
+        fetchNextPage()
+      }, function done(error) {
+        // Throw error if exists
+        if (error) reject({ error })
+  
+        // Finish
+        resolve(chaptersRecord)
+      })
+    })
+}
+const getCategoryByChapter= (chapter)=>{
+    
+  return new Promise((resolve, reject) => {
+      // Setup empty array to store results
+      
+      const chaptersRecord = []
+  
+      // Query
+      const apiQuery = {
+        pageSize: 100,
+      }
+  
+      // Go get it!
+      base('Chapters').select(apiQuery).eachPage(function page(records, fetchNextPage) {
+        // This function (`page`) will get called for each page of records.
+        // The properties here would correspond to your records
+
+        records.forEach(record=> {
+          if(record.fields.Chapters==chapter){
+            const post ={
+              Categories: record.fields.CategoryName
+          }
+          chaptersRecord.push(post)
+          }
+          
+  
+          // Store each result in our empty array
+          
+        })
+  
+        fetchNextPage()
+      }, function done(error) {
+        // Throw error if exists
+        if (error) reject({ error })
+  
+        // Finish
+        resolve(chaptersRecord)
+      })
+    })
+}
+
 server.get('/', (req, res) => {
     res.send("YWCA");
 });
+server.get('/chapters/services', (req,res)=>{
+  console.log(req.headers.chapter)
+  Promise.resolve(getServiceByChapter(req.headers.chapter)).then(data=>{
+    res.status(200).json(data)
+  }).catch(error=>{
+    res.status(500).json({message:error})
+  })
+})
+server.get('/chapters/categories', (req,res)=>{
+  Promise.resolve(getCategoryByChapter(req.headers.chapter)).then(data=>{
+    res.status(200).json(data)
+  }).catch(error=>{
+    res.status(500).json({message:error})
+  })
+})
+server.get('/state/chapters', (req,res)=>{
+  Promise.resolve(getChapterByState(req.headers.state)).then(data=>{
+    res.status(200).json(data)
+  }).catch(error=>{
+    res.status(500).json({message:error})
+  })
+})
 server.get('/chapters', (req, res) => {
     Promise.resolve(getRecords()).then(data=>{
         res.status(200).json(data)
